2005a685e1abe78a994f2c700a7dc7d6cdead5b1
Sandy1302 <hechisera_m@hotmail.com>
2015-11-17 17:39:52 -0600

    :warning:

--- ModaArreglos.c
+++ ModaArreglos.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
 int calificacion[10];
+int moda[10];
+int moda1;
 int i=0;
-float a,moda=0;
+
 int main()
 {
 	for (i=0;i<10;i++)
 	{
 		printf("Ingresa tu calificacion:");
 		scanf("%d",&calificacion[i]);
-		moda+=calificacion[i];
+		moda[i]=0;
 		
-		if(i==9)
+		for(i=0; i<10; i++)
 		{
-			a=calificacion[i];
-			printf("Moda:%d\n\n",a);
-			break;
+			moda[i]=0;
+			for(i=0; i<10; i++)
+			{
+				moda1=(calificacion[i]-1);
+				printf("La moda es %d",moda1);
+			}
 		}
 	}
 }
